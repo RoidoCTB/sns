9a19086a6d1d383b07786c22f038ecf7aa54e62b
RoidoCTB <lloydhhannessanchez@gmail.com>
2024-12-10 00:25:57 +0800

    added some changes on sql and db structure folder ref

--- db.sql
+++ db.sql
@@ -1,122 +1,48 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 23, 2019 at 03:00 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.1.30
+-- Create the database
+CREATE DATABASE socialmediadb;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Use the database
+USE socialmediadb;
 
+-- Create 'users' table
+CREATE TABLE users (
+    user_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(144) NOT NULL,
+    display_name VARCHAR(50) NOT NULL,
+    join_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    prof_bio VARCHAR(144),
+    prof_pic VARCHAR(512),
+    role ENUM('user', 'admin') DEFAULT 'user'
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Create 'posts' table
+CREATE TABLE posts (
+    post_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    post_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    post VARCHAR(1024) NOT NULL,
+    submittedby VARCHAR(50) NOT NULL,
+    rating INT(11),
+    image_path VARCHAR(255),
+    is_admin_post TINYINT(1) DEFAULT 0
+);
 
---
--- Database: `socialmediadb`
---
-CREATE DATABASE IF NOT EXISTS `socialmediadb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `socialmediadb`;
+-- Create 'comments' table
+CREATE TABLE comments (
+    comment_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    post_id INT(100) NOT NULL,
+    submittedby VARCHAR(512) NOT NULL,
+    comment VARCHAR(1024) NOT NULL,
+    comment_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE
+);
 
--- --------------------------------------------------------
+-- Create 'likes' table
+CREATE TABLE likes (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    post_id INT(11) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE
+);
 
---
--- Table structure for table `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (
-  `comment_id` int(11) NOT NULL,
-  `post_id` int(100) NOT NULL,
-  `submittedby` varchar(512) NOT NULL,
-  `comment` varchar(1024) NOT NULL,
-  `comment_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE `posts` (
-  `post_id` int(11) NOT NULL,
-  `post_date` datetime NOT NULL,
-  `post` varchar(1024) NOT NULL,
-  `submittedby` varchar(50) NOT NULL,
-  `rating` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(144) NOT NULL,
-  `display_name` varchar(50) NOT NULL,
-  `join_date` datetime NOT NULL,
-  `prof_bio` varchar(144) DEFAULT NULL,
-  `prof_pic` varchar(512) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`comment_id`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`post_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
